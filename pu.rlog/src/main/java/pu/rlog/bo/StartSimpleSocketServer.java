72efcf007f5c482e6e6492bfdeb83aeceab46098
peter <purbanus@gmail.com>
2024-10-10 19:36:59 +0200

    Gekopieerd van logback

---
+++ pu.rlog/src/main/java/pu/rlog/bo/StartSimpleSocketServer.java
@@ -0,0 +1,35 @@
+package pu.rlog.bo;
+
+import java.io.File;
+import java.net.URL;
+
+import ch.qos.logback.classic.net.SimpleSocketServer;
+
+public class StartSimpleSocketServer
+{
+/**
+ * java ch.qos.logback.classic.net.SimpleSocketServer 6000 \
+  src/main/java/chapters/appenders/socket/server1.xml
+ * @throws Exception 
+ */
+
+public static void main( String [] args ) throws Exception
+{
+	String path = "src/main/resources/logback-server.xml";
+//	URL url = StartSimpleSocketServer.class.getResource( path );
+	File file = new File( path);
+	if ( ! file.exists() )
+	{
+		System.err.println( "File bestaat niet!" );
+	}
+	else
+	{
+		String absolutePath = file.getAbsolutePath();
+		System.out.println( "Absolute path=" + absolutePath );
+		args = new String []{ "40000", absolutePath } ;
+		SimpleSocketServer.main( args );
+	}
+
+}
+
+}
